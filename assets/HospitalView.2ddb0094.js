928a667297c76dbc21cf0115d6c90681e3786224
JohnTing <jot.johnting@gmail.com>
2022-06-22 14:59:26 +0800

    Updates

---
+++ assets/HospitalView.2ddb0094.js
@@ -0,0 +1,2 @@
+import{d as o,o as t,c as a,e as _,_ as r}from"./index.dc5f7c1c.js";const p=o({__name:"HospitalView",setup(n){const e=()=>r(()=>import("./HospitalFrom.827c54da.js"),["assets/HospitalFrom.827c54da.js","assets/papaparse.min.2786dcc2.js","assets/_commonjsHelpers.4e997714.js","assets/index.dc5f7c1c.js","assets/index.4619ad6b.css"]);return(s,c)=>(t(),a("main",null,[_(e)]))}});export{p as default};
+//# sourceMappingURL=HospitalView.2ddb0094.js.map
